52b4ac31452b74c557e7a95cad1b29291fd8a5da
Anil Duri <anilduri.a@gmail.com>
2023-11-10 16:40:51 +0300

    some better spacing

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,7 +39,7 @@ class Home extends StatelessWidget {
                   ],
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     HomeButton(
                       icon: Icons.help,
