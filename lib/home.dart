aa101fd381c8c7e1bdde76a3ec40967983f8ccbf
Anil Duri <anilduri.a@gmail.com>
2023-11-10 13:50:52 +0300

    create home button widget

--- lib/home.dart
+++ lib/home.dart
@@ -10,17 +10,77 @@ class Home extends StatelessWidget {
       body: Center(
         child: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                child: GestureDetector(
-                  child: Text('data'),
-                ),
+              GestureDetector(
+                child: Text('data'),
               ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    HomeButton(
+                      icon: Icons.help,
+                      label: 'Tutorial',
+                      onTap: () {},
+                    ),
+                    HomeButton(
+                      icon: Icons.brush,
+                      label: 'Color',
+                      onTap: () {},
+                    ),
+                    HomeButton(
+                      icon: Icons.settings,
+                      label: 'Settings',
+                      onTap: () {},
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomeButton extends StatelessWidget {
+  const HomeButton(
+      {super.key,
+      required this.icon,
+      required this.label,
+      required this.onTap});
+
+  final IconData icon;
+  final String label;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 40,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            label,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
       ),
     );
   }
