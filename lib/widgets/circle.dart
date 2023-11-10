52f23d1abe0f911df4247b8b0d357e2d623f2683
Anil Duri <anilduri.a@gmail.com>
2023-11-10 15:29:40 +0300

    add animation when touching screen

--- lib/widgets/circle.dart
+++ lib/widgets/circle.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Circle extends StatelessWidget {
+class Circle extends StatefulWidget {
   const Circle({
     super.key,
   });
 
   @override
+  State<Circle> createState() => _CircleState();
+}
+
+class _CircleState extends State<Circle> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this)
+      ..forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Icon(
-      Icons.circle_outlined,
-      size: 150,
-      color: Colors.white,
+    return ScaleTransition(
+      scale: CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastEaseInToSlowEaseOut,
+      ),
+      child: const Icon(
+        Icons.circle_outlined,
+        size: 150,
+        color: Colors.white,
+      ),
     );
   }
 }
