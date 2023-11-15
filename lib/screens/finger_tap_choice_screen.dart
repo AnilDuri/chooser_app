9093313ef96bd76594c0bf28cb64d0f201b75391
Anil Duri <anilduri.a@gmail.com>
2023-11-15 14:19:28 +0300

    update home screen layout

---
+++ lib/screens/finger_tap_choice_screen.dart
@@ -0,0 +1,63 @@
+import 'package:chooser_ap/screens/home.dart';
+import 'package:chooser_ap/widgets/circle.dart';
+import 'package:chooser_ap/widgets/touch_indicator.dart';
+import 'package:flutter/material.dart';
+
+class FingerTapChoiceScreen extends StatelessWidget {
+  const FingerTapChoiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TouchIndicator(
+      indicator: const Circle(),
+      indicatorSize: 150,
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Column(
+              children: [
+                Text(
+                  'Chooser 2.0',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Your random selector.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                HomeButton(
+                  icon: Icons.help,
+                  label: 'Tutorial',
+                  onTap: () {},
+                ),
+                HomeButton(
+                  icon: Icons.brush,
+                  label: 'Color',
+                  onTap: () {},
+                ),
+                HomeButton(
+                  icon: Icons.settings,
+                  label: 'Settings',
+                  onTap: () {},
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,66 +1,84 @@
-import 'package:chooser_ap/widgets/circle.dart';
-import 'package:chooser_ap/widgets/touch_indicator.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _choiceController = 'finger';
+
+  @override
   Widget build(BuildContext context) {
-    return TouchIndicator(
-      indicator: const Circle(),
-      indicatorSize: 150,
-      child: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Column(
-                  children: [
-                    Text(
-                      'Chooser 2.0',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold),
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: height * 0.85,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Chooser 2.0',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 46,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    'Your random selector.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  const SizedBox(height: 20),
+                  DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      value: _choiceController,
+                      items: const [
+                        DropdownMenuItem(
+                            value: 'finger', child: Text('Finger Chooser')),
+                        DropdownMenuItem(
+                            value: 'shaker', child: Text('Shaker Chooser')),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _choiceController = value!;
+                        });
+                      },
+                      dropdownColor: Colors.amber,
+                      elevation: 0,
+                      icon: const Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
                     ),
-                    Text(
-                      'Your random selector.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w300),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    HomeButton(
-                      icon: Icons.help,
-                      label: 'Tutorial',
-                      onTap: () {},
-                    ),
-                    HomeButton(
-                      icon: Icons.brush,
-                      label: 'Color',
-                      onTap: () {},
-                    ),
-                    HomeButton(
-                      icon: Icons.settings,
-                      label: 'Settings',
-                      onTap: () {},
-                    )
-                  ],
-                )
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  minimumSize: const Size(150, 40)),
+              child: const Text(
+                'Continue',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          ],
         ),
       ),
     );
