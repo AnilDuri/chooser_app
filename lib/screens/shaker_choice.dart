feed8bd1ae4783153b277d2fb80dd69b4bf1e6f4
Anil Duri <anilduri.a@gmail.com>
2023-11-16 11:13:19 +0300

    fix text for name

--- lib/screens/shaker_choice.dart
+++ lib/screens/shaker_choice.dart
@@ -13,56 +13,61 @@ class _ShakerChoiceScreen extends State<ShakerChoiceScreen> {
   final _nameController = TextEditingController();
   final List<String> _names = [];
 
+  void _savePerson() {
+    final enteredName = _nameController.text;
+    if (enteredName.isEmpty) {
+      return;
+    }
+    if (_names.contains(enteredName)) {}
+    setState(() {
+      _names.add(_nameController.text);
+    });
+
+    _nameController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shaker'),
+      ),
       backgroundColor: Colors.amberAccent,
-      body: Center(
-          child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Add people to your list and start shaking to determing who remains',
-              textAlign: TextAlign.center,
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Title'),
-              controller: _nameController,
-              style:
-                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                final enteredName = _nameController.text;
-                if (enteredName.isEmpty) {
-                  return;
-                }
-                setState(() {
-                  _names.add(_nameController.text);
-                });
-
-                _nameController.clear();
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  minimumSize: const Size(150, 40)),
-              child: const Text(
-                'Add Person',
-                style: TextStyle(fontSize: 18),
+      body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Add people to your list and start shaking to determe the starting order',
+                textAlign: TextAlign.center,
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: _names.length,
-                  itemBuilder: (ctx, index) => ListTile(
-                        title: Text(_names[index]),
-                      )),
-            )
-          ],
-        ),
-      )),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                controller: _nameController,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: _savePerson,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    minimumSize: const Size(150, 40)),
+                child: const Text(
+                  'Add Person',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _names.length,
+                    itemBuilder: (ctx, index) => ListTile(
+                          title: Text(_names[index]),
+                        )),
+              )
+            ],
+          )),
     );
   }
 }
